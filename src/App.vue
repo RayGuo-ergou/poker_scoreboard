906fa7821c07accb68336360d542894d054d488a
Ray Guo <ma2808203259@hotmail.com>
2022-07-19 13:23:58 +1000

    remove result json from website

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,102 @@
 <template>
-    <v-container>
-        <div v-show="!inGame">
-            <h2>
-                Enter players name.
-                <i>Type and hit entry if your name not in the list</i>
-            </h2>
-            <InputName ref="inputName" @submit="submit"></InputName>
-        </div>
+  <v-container>
+    <div v-show="!inGame">
+      <h2>
+        Enter players name.
+        <i>Type and hit entry if your name not in the list</i>
+      </h2>
+      <InputName ref="inputName" @submit="submit"></InputName>
+    </div>
 
-        <div v-if="inGame">
-            <v-row>
-                <v-col
-                    v-for="play in players"
-                    :key="play"
-                    cols="8"
-                    lg="6"
-                    md="6"
-                    xl="3"
-                    ><Score
-                        @result="result"
-                        ref="scoreboard"
-                        :username="play"
-                    ></Score>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="6">
-                    <v-btn @click="addCards" color="primary">add all</v-btn>
-                </v-col>
-                <v-col cols="6">
-                    <v-btn @click="sendResult" color="info">result</v-btn>
-                </v-col>
-            </v-row>
-            <v-row justify="center">
-                <v-col>
-                    <v-btn @click="close" v-show="resultSeen" color="error"
-                        >End Game</v-btn
-                    >
-                </v-col>
-            </v-row>
-        </div>
-        <test></test>
-    </v-container>
+    <div v-if="inGame">
+      <v-row>
+        <v-col v-for="play in players" :key="play" cols="8" lg="6" md="6" xl="3"
+          ><Score @result="result" ref="scoreboard" :username="play"></Score>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-btn @click="addCards" color="primary">add all</v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-btn @click="sendResult" color="info">result</v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col>
+          <v-btn @click="close" v-show="resultSeen" color="error"
+            >End Game</v-btn
+          >
+        </v-col>
+      </v-row>
+    </div>
+    <test></test>
+  </v-container>
 </template>
 
 <script>
-import Score from "./components/Score.vue";
-import InputName from "./components/InputName.vue";
+import Score from './components/Score.vue';
+import InputName from './components/InputName.vue';
 
 export default {
-    name: "App",
+  name: 'App',
 
-    components: {
-        Score,
-        InputName,
+  components: {
+    Score,
+    InputName,
+  },
+
+  data: () => ({
+    inGame: $cookies.isKey('names') ? true : false,
+    players: $cookies.isKey('names') ? JSON.parse($cookies.get('names')) : [],
+    finalResult: [],
+    resultSeen: false,
+  }),
+
+  methods: {
+    submit(names) {
+      this.players = names;
+      this.inGame = true;
     },
-
-    data: () => ({
-        inGame: $cookies.isKey("names") ? true : false,
-        players: $cookies.isKey("names")
-            ? JSON.parse($cookies.get("names"))
-            : [],
-        finalResult: [],
-        resultSeen: false,
-    }),
-
-    methods: {
-        submit(names) {
-            this.players = names;
-            this.inGame = true;
-        },
-        addCards() {
-            // add a event to score child
-            // for each of the score child, add a card
-            this.$refs.scoreboard.forEach((element) => {
-                element.addScore();
-            });
-        },
-        // reset the game
-        close() {
-            this.players = [];
-            this.finalResult = [];
-            this.$refs.inputName.close();
-            this.inGame = false;
-            this.resultSeen = false;
-            // remove all cookie
-            $cookies.keys().forEach((cookie) => $cookies.remove(cookie));
-        },
-        // finish the game calculate the result
-        sendResult() {
-            this.finalResult = [];
-            this.$refs.scoreboard.forEach((element) => {
-                element.sendResult();
-            });
-        },
-        result(result) {
-            this.finalResult.push(result);
-            if (this.finalResult.length === this.players.length) {
-                this.$refs.scoreboard.forEach((element) => {
-                    console.log(this.finalResult);
-                    element.calculateResult(this.finalResult);
-                });
-            }
-            this.resultSeen = true;
-        },
+    addCards() {
+      // add a event to score child
+      // for each of the score child, add a card
+      this.$refs.scoreboard.forEach((element) => {
+        element.addScore();
+      });
     },
+    // reset the game
+    close() {
+      this.players = [];
+      this.finalResult = [];
+      this.$refs.inputName.close();
+      this.inGame = false;
+      this.resultSeen = false;
+      // remove all cookie
+      $cookies.keys().forEach((cookie) => $cookies.remove(cookie));
+    },
+    // finish the game calculate the result
+    sendResult() {
+      this.finalResult = [];
+      this.$refs.scoreboard.forEach((element) => {
+        element.sendResult();
+      });
+    },
+    result(result) {
+      this.finalResult.push(result);
+      if (this.finalResult.length === this.players.length) {
+        this.$refs.scoreboard.forEach((element) => {
+          console.log(this.finalResult);
+          element.calculateResult(this.finalResult);
+        });
+      }
+      this.resultSeen = true;
+    },
+  },
 };
 </script>
 <style scoped>
 h2 {
-    text-align: center;
+  text-align: center;
 }
 </style>
--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,137 +1,127 @@
 <template>
-    <v-container>
-        <h3>{{ username }}</h3>
-        <h4>Score</h4>
-        <v-row>
-            <v-col cols="9">
-                <v-form v-model="valid">
-                    <v-text-field
-                        @focus="$event.target.select()"
-                        type="number"
-                        label="Main input"
-                        hide-details="auto"
-                        v-model="temp"
-                        :rules="rules"
-                        :min="0"
-                        :max="13"
-                    ></v-text-field>
-                </v-form>
-            </v-col>
-            <v-col cols="2"
-                ><v-btn @click="addScore" color="primary">Add</v-btn></v-col
-            >
-        </v-row>
+  <v-container>
+    <h3>{{ username }}</h3>
+    <h4>Score</h4>
+    <v-row>
+      <v-col cols="9">
+        <v-form v-model="valid">
+          <v-text-field
+            @focus="$event.target.select()"
+            type="number"
+            label="Main input"
+            hide-details="auto"
+            v-model="temp"
+            :rules="rules"
+            :min="0"
+            :max="13"
+          ></v-text-field>
+        </v-form>
+      </v-col>
+      <v-col cols="2"
+        ><v-btn @click="addScore" color="primary">Add</v-btn></v-col
+      >
+    </v-row>
 
-        <v-dialog v-model="dialog" width="500">
-            <v-card>
-                <v-card-title color="error" class="text-h5 grey lighten-2"
-                    >Error</v-card-title
-                >
-                <v-card-text>
-                    <v-container>
-                        {{ username }} has invalid input. (should between 1 to
-                        65)
-                    </v-container>
-                </v-card-text>
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title color="error" class="text-h5 grey lighten-2"
+          >Error</v-card-title
+        >
+        <v-card-text>
+          <v-container>
+            {{ username }} has invalid input. (should between 1 to 65)
+          </v-container>
+        </v-card-text>
 
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" text @click="dialog = false"
-                        >Okay</v-btn
-                    >
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">Okay</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-        <p>{{ score }}</p>
-        <p>
+    <p>{{ score }}</p>
+    <!-- <p>
             {{ finalResult }}
-        </p>
-        <div v-html="calPayments"></div>
-    </v-container>
+        </p> -->
+    <div v-html="calPayments"></div>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            score: $cookies.isKey(this.username)
-                ? parseInt($cookies.get(this.username))
-                : 0,
-            temp: 0,
-            leftCards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-            rules: [
-                (v) => v >= 0 || "Error:min 0",
-                (v) => v <= 65 || "Error:max 65",
-            ],
-            valid: true,
-            dialog: false,
-            finalResult: [],
-        };
+  data() {
+    return {
+      score: $cookies.isKey(this.username)
+        ? parseInt($cookies.get(this.username))
+        : 0,
+      temp: 0,
+      leftCards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      rules: [(v) => v >= 0 || 'Error:min 0', (v) => v <= 65 || 'Error:max 65'],
+      valid: true,
+      dialog: false,
+      finalResult: [],
+    };
+  },
+  computed: {
+    calPayments() {
+      let str = '';
+      this.finalResult.forEach((element) => {
+        if (element.score > 0) {
+          str +=
+            '<p>Paying ' + element.username + ': ' + element.score + '</p>';
+        }
+      });
+      return str;
     },
-    computed: {
-        calPayments() {
-            let str = "";
-            this.finalResult.forEach((element) => {
-                if (element.score > 0) {
-                    str +=
-                        "<p>Paying " +
-                        element.username +
-                        ": " +
-                        element.score +
-                        "</p>";
-                }
-            });
-            return str;
-        },
-    },
+  },
 
-    props: {
-        username: {
-            type: String,
-            required: true,
-        },
+  props: {
+    username: {
+      type: String,
+      required: true,
     },
-    methods: {
-        addScore() {
-            // if valid is false
-            // alert error
-            if (this.valid || this.temp === 0) {
-                this.score += parseInt(this.temp);
-                // set cookie
-                $cookies.set(this.username, this.score);
-                this.temp = 0;
-            } else {
-                this.dialog = true;
-            }
+  },
+  methods: {
+    addScore() {
+      // if valid is false
+      // alert error
+      if (this.valid || this.temp === 0) {
+        this.score += parseInt(this.temp);
+        // set cookie
+        $cookies.set(this.username, this.score);
+        this.temp = 0;
+      } else {
+        this.dialog = true;
+      }
 
-            this.finalResult = [];
-        },
-        sendResult() {
-            this.$emit("result", {
-                username: this.username,
-                score: this.score,
-            });
-        },
-        calculateResult(array) {
-            // let currentScore = 0;
-            this.finalResult = [];
-            // array.forEach((element) => {
-            //     // if element.username = this.username continue
-            //     if (element.username === this.username) {
-            //         currentScore = element.score;
-            //         return;
-            //     }
-            // });
-            array.forEach((element) => {
-                if (element.username !== this.username) {
-                    this.finalResult.push({
-                        username: element.username,
-                        score: this.score - element.score,
-                    });
-                }
-            });
-        },
+      this.finalResult = [];
     },
+    sendResult() {
+      this.$emit('result', {
+        username: this.username,
+        score: this.score,
+      });
+    },
+    calculateResult(array) {
+      // let currentScore = 0;
+      this.finalResult = [];
+      // array.forEach((element) => {
+      //     // if element.username = this.username continue
+      //     if (element.username === this.username) {
+      //         currentScore = element.score;
+      //         return;
+      //     }
+      // });
+      array.forEach((element) => {
+        if (element.username !== this.username) {
+          this.finalResult.push({
+            username: element.username,
+            score: this.score - element.score,
+          });
+        }
+      });
+    },
+  },
 };
 </script>
